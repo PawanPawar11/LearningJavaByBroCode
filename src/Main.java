59f017669c0e64e3d2ee69a7e6359a7f1cf9a2c8
PawanPawar11 <pawanpawar4369@gmail.com>
2025-07-27 17:47:09 +0530

    sixth job completed!

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,28 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-//        Arithmetic Operators
-//        int x = 10;
-//        int y = 2;
-//        int z;
-//
-//        System.out.println(x + y);
-//        System.out.println(x - y);
-//        System.out.println(x * y);
-//        System.out.println(x / y);
-//        System.out.println(x % y);
+        Scanner scanner = new Scanner(System.in);
 
-//        Augmented Assignment Operator
-//        int x = 10;
-//        int y = 2;
-//
-//        x += y;
-//        x -= y;
-//        x *= y;
-//        x /= y;
-//        x %= y;
-//        System.out.println(x);
+        String item;
+        double price;
+        int quantity;
+        double total;
+        char currency = '$';
 
-//        Increment and Decrement Operator
-//        int x = 1;
-//        x++;
-//        x++;
-//        x++;
-//
-//        x--;
-//        x--;
-//        System.out.println(x);
+        System.out.print("What item would you like to buy?: ");
+        item = scanner.nextLine();
 
-//        ORDER OF OPERATION (P-E-M-D-A-S)
-//        Please Excuse My Dear Aunt Sally
-//        Please Excuse My Dope Ass Swag 🤣
-//        P -> Parentheses
-//        E -> Exponent
-//        M -> Multiplication
-//        D -> Division
-//        A -> Addition
-//        S -> Subtraction
-//        double result = 3 + 4 * (7 - 5) / 2.0;
-//        // 3 + 4 * (2) / 2.0
-//        // 3 + 8 / 2.0
-//        // 3 + 4.0
-//        // 7.0
-//        System.out.println(result);
+        System.out.print("What is the price for each?: ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many would you like: ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+        System.out.println("\nYou have bought " + quantity + " " + item + "/s");
+        System.out.println("Your total is: " + currency + total);
+
+        scanner.close();
     }
 }
