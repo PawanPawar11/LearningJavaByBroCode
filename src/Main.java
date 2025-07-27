6772b98ed4fa9666718625056d5078722f681736
PawanPawar11 <pawanpawar4369@gmail.com>
2025-07-27 10:57:41 +0530

    second job completed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Above all, don't lie to yourself.");
-        System.out.println("The man who lies to himself and listens to his own lie");
-        System.out.println("comes to a point that he cannot distinguish the truth within him, or around him.");
+    public  static  void main(String[] args) {
+        String name = "Ash";
+        char currency = '$';
+        int annualSalary = 100_000;
+        String car = "Ford Mustang";
+        boolean isASoftwareEngineer = false;
+
+        System.out.println("Your name is " + name);
+        System.out.println("Your annual salary is " + currency + annualSalary);
+        System.out.println("You own the car " + car);
+        System.out.println("You're a software engineer: " + isASoftwareEngineer);
     }
 }
