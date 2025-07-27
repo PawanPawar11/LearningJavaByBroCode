c4e82b2ee95e4bea183d94e0a69e2cab5eaf7c06
PawanPawar11 <pawanpawar4369@gmail.com>
2025-07-27 13:18:50 +0530

    fourth job completed!

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-    public  static  void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String name;
-        boolean isStudent = false;
-        String favoriteSubject;
-        double cgpa;
-        char grade;
+        String character;
+        int number;
+        String saysSomething;
+        boolean trueOrFalse;
 
-        System.out.print("What is your name: ");
-        name = scanner.nextLine();
+        System.out.print("Enter a character (name): ");
+        character = scanner.nextLine();
 
-        System.out.print("Are you a student? (true/false): ");
-        isStudent = scanner.nextBoolean();
-        scanner.nextLine();
+        System.out.print("Enter a number: ");
+        number = scanner.nextInt();
+        scanner.nextLine(); // Clear the buffer
 
-        System.out.print("Which is your favorite subject: ");
-        favoriteSubject = scanner.nextLine();
+        System.out.print("Enter something the character says: ");
+        saysSomething = scanner.nextLine();
 
-        System.out.print("Enter your cgpa: ");
-        cgpa = scanner.nextDouble();
+        System.out.print("Did it really happen? (true/false): ");
+        trueOrFalse = scanner.nextBoolean();
 
-        System.out.print("Enter your favorite subject's grade: ");
-        grade = scanner.next().charAt(0);
-
-        System.out.println("\nYour name is: " + name);
-        System.out.println("You are a student: " + isStudent);
-        System.out.println("Your favorite subject is: " + favoriteSubject);
-        System.out.println("Your cgpa is: " + cgpa);
-        System.out.println("Your favorite subject's grade is: " + grade);
+        System.out.println("\nOne morning, " + character + " went for a walk.");
+        System.out.println("They found " + number + " small boxes under a tree.");
+        System.out.println(saysSomething + "!" + " they said.");
+        System.out.println("A dog nearby started barking loudly.");
+        System.out.println("Was all of this real? " + trueOrFalse + ".");
+        System.out.println("It was a strange day.");
     }
 }
