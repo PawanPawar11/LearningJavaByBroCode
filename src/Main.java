96e104b5d7286b0326a04395fa57377ff47e9412
PawanPawar11 <pawanpawar4369@gmail.com>
2025-07-27 11:57:34 +0530

    third job completed!

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public  static  void main(String[] args) {
-        String name = "Ash";
-        char currency = '$';
-        int annualSalary = 100_000;
-        String car = "Ford Mustang";
-        boolean isASoftwareEngineer = false;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Your name is " + name);
-        System.out.println("Your annual salary is " + currency + annualSalary);
-        System.out.println("You own the car " + car);
-        System.out.println("You're a software engineer: " + isASoftwareEngineer);
+        String name;
+        boolean isStudent = false;
+        String favoriteSubject;
+        double cgpa;
+        char grade;
+
+        System.out.print("What is your name: ");
+        name = scanner.nextLine();
+
+        System.out.print("Are you a student? (true/false): ");
+        isStudent = scanner.nextBoolean();
+        scanner.nextLine();
+
+        System.out.print("Which is your favorite subject: ");
+        favoriteSubject = scanner.nextLine();
+
+        System.out.print("Enter your cgpa: ");
+        cgpa = scanner.nextDouble();
+
+        System.out.print("Enter your favorite subject's grade: ");
+        grade = scanner.next().charAt(0);
+
+        System.out.println("\nYour name is: " + name);
+        System.out.println("You are a student: " + isStudent);
+        System.out.println("Your favorite subject is: " + favoriteSubject);
+        System.out.println("Your cgpa is: " + cgpa);
+        System.out.println("Your favorite subject's grade is: " + grade);
     }
 }
