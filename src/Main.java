e0cdc31961018b5c254036916dcf82e313bcf777
PawanPawar11 <pawanpawar4369@gmail.com>
2025-07-29 12:17:24 +0530

    seventh job completed!

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,49 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        int age;
+        String name;
+        boolean isStudent;
+
         Scanner scanner = new Scanner(System.in);
 
-        String item;
-        double price;
-        int quantity;
-        double total;
-        char currency = '$';
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
 
-        System.out.print("What item would you like to buy?: ");
-        item = scanner.nextLine();
+        scanner.nextLine();
 
-        System.out.print("What is the price for each?: ");
-        price = scanner.nextDouble();
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine();
 
-        System.out.print("How many would you like: ");
-        quantity = scanner.nextInt();
+        System.out.print("Are you a student? (true/false): ");
+        isStudent = scanner.nextBoolean();
 
-        total = price * quantity;
+        // Block 1
+        if (age >= 65) {
+            System.out.println("You're a senior! 👴");
+        }
+        else if(age >= 18) {
+            System.out.println("You are an adult! 🧑");
+        } else if (age < 18) {
+            System.out.println("You're a child! 🧒");
+        } else if (age == 0) {
+            System.out.println("You are a baby! 👶");
+        } else {
+            System.out.println("You haven't been born yet! 👼");
+        }
 
-        System.out.println("\nYou have bought " + quantity + " " + item + "/s");
-        System.out.println("Your total is: " + currency + total);
+        // Block 2
+        if(name.isEmpty()) {
+            System.out.println("You didn't enter your name! 😡");
+        } else {
+            System.out.println("Hello " + name);
+        }
 
-        scanner.close();
+        // Block 3
+        if(isStudent) {
+            System.out.println("You're a student! 🏫");
+        } else {
+            System.out.println("You're NOT a student! 🏢");
+        }
     }
 }
